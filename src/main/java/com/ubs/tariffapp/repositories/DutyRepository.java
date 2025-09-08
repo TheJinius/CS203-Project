b9e43a05aee87fb2f33c3bc67332acc2ef4da771
Neo Jia Wen <neojiawen05@gmail.com>
2025-09-08 16:41:46 +0800

    Deleted DutyRepository as Duty is abstract

--- src/main/java/com/ubs/tariffapp/repositories/DutyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ubs.tariffapp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ubs.tariffapp.models.Duty;
-
-public interface DutyRepository extends JpaRepository<Duty, Integer> {
-}
