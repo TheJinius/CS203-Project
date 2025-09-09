5d20d8f26ce6df0bc40c87e1af42ea30bf0e6671
Neo Jia Wen <neojiawen05@gmail.com>
2025-09-09 23:19:54 +0800

    Removed unused imports

--- src/test/java/com/ubs/tariffapp/repositories/DutyTypeRepositoryTest.java
+++ src/test/java/com/ubs/tariffapp/repositories/DutyTypeRepositoryTest.java
@@ -1,8 +1,5 @@
 package com.ubs.tariffapp.repositories;
 
-import java.util.Collections;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
