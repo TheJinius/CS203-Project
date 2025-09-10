ff5360119d05a6abad983b9e8086f77ad04cbf7d
Ethan Lim <limjinethan@gmail.com>
2025-09-11 01:57:55 +0800

    Added calculation logic to DutyService

--- src/main/java/com/ubs/tariffapp/Services/DutyService.java
+++ src/main/java/com/ubs/tariffapp/Services/DutyService.java
@@ -2,7 +2,10 @@ package com.ubs.tariffapp.Services;
 
 import org.springframework.stereotype.Service;
 import com.ubs.tariffapp.repositories.DutyRepository;
+import com.ubs.tariffapp.models.AdValoremDuty;
+import com.ubs.tariffapp.models.CombinedDuty;
 import com.ubs.tariffapp.models.Duty;
+import com.ubs.tariffapp.models.SpecificDuty;
 
 
 @Service
@@ -24,7 +27,32 @@ public class DutyService {
     public String calculateTariff(String importerCountry, String exporterCountry, String product) {
         Duty duty = getDuty(importerCountry, exporterCountry, product);
         // Add your calculation logic here using the Duty object
+        if (duty == null) {
+            return "No applicable duty found."; // or should i return null or throw exception?
+        }
         
+        if (duty instanceof AdValoremDuty) {
+            AdValoremDuty adValoremDuty = (AdValoremDuty) duty;
+            return "Ad Valorem Duty Rate: " + adValoremDuty.getRatePercent() + "%";
+        } else if (duty instanceof SpecificDuty) {
+            SpecificDuty specificDuty = (SpecificDuty) duty;
+            return "Specific Duty Amount: " + specificDuty.getAmount() + " per " + specificDuty.getMultiplier() + " " + specificDuty.getUnit();
+            
+        } else if (duty instanceof CombinedDuty) {
+            CombinedDuty combinedDuty = (CombinedDuty) duty;
+            if (combinedDuty.getMixedOrConditional().equals("M")) {
+                return "Combined Duty (Mixed): " +
+                        "Ad Valorem Rate: " + combinedDuty.getRatePercent() + "%, " +
+                        "Specific Amount: " + combinedDuty.getAmount() + " per " + combinedDuty.getMultiplier() + " " + combinedDuty.getUnit();
+            } else if (combinedDuty.getMixedOrConditional().equals("C")) {
+                return "Combined Duty (Conditional): " +
+                        "Ad Valorem Rate: " + combinedDuty.getRatePercent() + "%, " +
+                        "Specific Amount: " + combinedDuty.getAmount() + " per " + combinedDuty.getMultiplier() + " " + combinedDuty.getUnit();
+                
+            }
+            
+        }
+
         return null;
     }
 }
