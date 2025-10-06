565cd4d9e178590d33d63312c2e7eb348ccd425c
Neo Jia Wen <neojiawen05@gmail.com>
2025-10-06 15:49:43 +0800

    Added more descriptive text for the bearer token auth in Swagger UI.

--- src/main/java/com/ubs/tariffapp/config/OpenApiConfig.java
+++ src/main/java/com/ubs/tariffapp/config/OpenApiConfig.java
@@ -1,15 +1,16 @@
 package com.ubs.tariffapp.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.OAuthFlows;
-import io.swagger.v3.oas.models.security.OAuthFlow;
-import io.swagger.v3.oas.models.security.Scopes;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 
 @Configuration
 @OpenAPIDefinition(
@@ -49,7 +50,7 @@ public class OpenApiConfig {
                 .addSecuritySchemes("oauth2",
                     new io.swagger.v3.oas.models.security.SecurityScheme()
                         .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.OAUTH2)
-                        .description("OAuth2 authentication via AWS Cognito (Implicit Flow).")
+                        .description("OAuth2 authentication via AWS Cognito. Step 1: Use this to authenticate with AWS Cognito and get your access token.")
                         .flows(new OAuthFlows()
                             .implicit(new OAuthFlow()
                                 .authorizationUrl(authorizationUrl)
@@ -64,7 +65,7 @@ public class OpenApiConfig {
                         .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
                         .scheme("bearer")
                         .bearerFormat("JWT")
-                        .description("Bearer token authentication. Paste your access token from OAuth2 flow here.")
+                        .description("Bearer token authentication. Step 2: Paste your access token here (get it from OAuth2 authentication above).")
                 )
             );
     }
