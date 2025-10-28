c4f2b29575803f6967d54b052612562a2a922069
Ethan <limjinethan@gmail.com>
2025-10-28 16:34:45 +0800

    commented out test for local

--- src/main/java/com/ubs/tariffapp/config/CorsConfig.java
+++ src/main/java/com/ubs/tariffapp/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
+    // test for local
     // @Override
     // public void addCorsMappings(CorsRegistry registry) {
     //     registry.addMapping("/**")
