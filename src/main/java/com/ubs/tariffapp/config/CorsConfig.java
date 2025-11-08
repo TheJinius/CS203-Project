09969557a5f09de33e0ef8f97824fea09b067d85
Neo Jia Wen <neojiawen05@gmail.com>
2025-11-08 22:47:07 +0800

    Changed cors config

--- src/main/java/com/ubs/tariffapp/config/CorsConfig.java
+++ src/main/java/com/ubs/tariffapp/config/CorsConfig.java
@@ -36,7 +36,7 @@ public class CorsConfig implements WebMvcConfigurer {
         configuration.addAllowedOrigin("http://localhost:8080");
         configuration.addAllowedOrigin("http://127.0.0.1:8080");
         configuration.addAllowedOrigin("https://cs203tariffproject.duckdns.org/api");
-        configuration.addAllowedOrigin("https://cs203chatbot.duckdns.org");
+        configuration.addAllowedOrigin("https://cs203chatbot.duckdns.org/query");
         configuration.addAllowedOrigin("https://trade-optimisation-pathfinder.vercel.app");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
